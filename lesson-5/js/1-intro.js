b153d55860871d8890045e23b43561a688ce81f6
Matthew Zarcone <81539632+northernliving4@users.noreply.github.com>
2026-06-08 18:36:30 -0400

    e

---
+++ lesson-5/js/1-intro.js
@@ -0,0 +1,12 @@
+async function startProgram() {
+
+    setMainLed(getRandomColor());
+
+    await scrollMatrixText("Hello World!", getRandomColor(), 24, false)
+
+    await roll(0,30,2);
+
+    exitProgram();
+
+
+}
